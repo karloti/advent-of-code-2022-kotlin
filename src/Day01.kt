28d7559dfe11c804fbe916a783dc9f93563d193e
Kaloyan Karaivanov <skarloti@gmail.com>
2022-12-01 17:25:04 +0200

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,19 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: String): Int = input
+        .split("\n\n")
+        .maxOf { it.split("\n").sumOf(String::toInt) }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: String): Int = input
+        .split("\n\n")
+        .map { it.split("\n").sumOf(String::toInt) }
+        .sortedDescending()
+        .take(3)
+        .sum()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInputAsText("Day01_test")
+//    check(part1(testInput) == 24000)
 
-    val input = readInput("Day01")
+    val input = readInputAsText("Day01")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,8 @@ import java.security.MessageDigest
 fun readInput(name: String) = File("src", "$name.txt")
     .readLines()
 
+fun readInputAsText(name: String) = File("src", "$name.txt").readText()
+
 /**
  * Converts string to md5 hash.
  */
