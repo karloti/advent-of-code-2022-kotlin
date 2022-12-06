7e41a7c06ec11799af263392381c26c79f730048
Kaloyan Karaivanov <skarloti@gmail.com>
2022-12-06 17:44:34 +0200

    Day06

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,18 +1,24 @@
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
+
 fun String.indexOf(n: Int): Int = asSequence()
     .withIndex()
-    .runningFold(mapOf<Char, Int>() to -1) { (map, result), (index, char) ->
-        Pair(map + (char to index), (map[char]?.let { maxOf(it, result) } ?: result))
+    .runningFold(mutableMapOf<Char, Int>() to -1) { (m, r), (i, c) ->
+        m to (m.put(c, i)?.coerceAtLeast(r) ?: r)
     }
     .withIndex()
     .indexOfFirst { (index, value) -> index - value.second - 1 == n }
 
-fun main() {
-    check("mjqjpqmgbljsphdztnvjfqwrcgsmlb".indexOf(4).also(::println) == 7)
-    check("bvwbjplbgvbhsrlpgdmjqwftvncz".indexOf(4).also(::println) == 5)
-    check("nppdvjthqldpwncqszvftbrmjlhg".indexOf(4).also(::println) == 6)
-    check("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".indexOf(4).also(::println) == 10)
-    check("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".indexOf(4).also(::println) == 11)
 
-    check(readInputAsText("Day06").indexOf(4).also(::println) == 1909)
-    check(readInputAsText("Day06").indexOf(14).also(::println) == 3380)
+@OptIn(ExperimentalTime::class)
+fun main() {
+    val s = ('a'..'y').joinToString("").repeat(1_000_000) + "z"
+    print("runningFold = ")
+    measureTimedValue { s.indexOf(26) }.let(::println)
+
+    print("windowed = ")
+    measureTimedValue { s.windowed(26).indexOfFirst { it.toSet().size == 26 } + 26 }.let(::println)
+
+    check(readInputAsText("Day06").indexOf(4) == 1909)
+    check(readInputAsText("Day06").indexOf(14) == 3380)
 }
