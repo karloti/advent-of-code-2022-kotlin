4c06870c045d9356950c2d8812c478a02eecd9f7
Kaloyan Karaivanov <skarloti@gmail.com>
2022-12-12 16:51:27 +0200

    Day12

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,29 +1,23 @@
 import java.util.ArrayDeque
 
-val valueByChar: Map<Char, Int> = ("S" + ('a'..'z').joinToString("") + "E").withIndex().associate { (k, v) -> v to k }
+val valueByChar: Map<Char, Int> = "SabcdefghijklmnopqrstuvwxyzE".withIndex().associate { (k, v) -> v to k }
+val directions = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to +1)
 
-enum class Direction(val h: Int, val v: Int) { L(-1, 0), R(1, 0), U(0, -1), D(0, 1) }
 data class Point(val h: Int, val v: Int, val c: Char, var d: Int? = null)
 
-fun List<String>.toPoints() = withIndex().map { (v, s) -> s.withIndex().map { (h, c) -> Point(h, v, c) } }
+fun List<String>.toPoints(): List<List<Point>> = mapIndexed { v, s -> s.mapIndexed { h, c -> Point(h, v, c) } }
 
-fun Point.adjacentPoints(points: List<List<Point>>, compare: (Int, Int) -> Boolean) =
-    Direction.values().mapNotNull {
-        (v + it.v to h + it.h)
-            .takeIf { (v1, h1) -> v1 in points.indices && h1 in points[0].indices }
-            ?.let { (v1, h1) -> points[v1][h1] }
-            ?.takeIf { compare(valueByChar[c]!!, valueByChar[it.c]!!) }
-    }
+fun Point.adjPoints(points: List<List<Point>>, compare: (Int, Int) -> Boolean): List<Point> = directions
+    .mapNotNull { (v1, h1) -> points.getOrNull(v + v1)?.getOrNull(h + h1) }
+    .filter { compare(valueByChar[c]!!, valueByChar[it.c]!!) }
 
-fun solution(input: List<String>, start: Char, end: Char, compare: (Int, Int) -> Boolean): Int? {
-    val points = input.toPoints()
-    val startFromPoint = points.flatten().first { it.c == start }
-    val queue = ArrayDeque<Point>().apply { add(startFromPoint.apply { d = 0 }) }
-    while (queue.isNotEmpty()) {
-        queue.removeFirst().apply {
-            if (c == end) return d
-            adjacentPoints(points, compare).filter { it.d == null }.forEach { it.d = d!! + 1; queue.addLast(it) }
-        }
+fun solution(input: List<String>, startChar: Char, endChar: Char, compare: (Int, Int) -> Boolean): Int? {
+    val points: List<List<Point>> = input.toPoints()
+    val startPoint: Point = points.flatten().first { it.c == startChar }
+    val queue = ArrayDeque<Point>().apply { add(startPoint.apply { d = 0 }) }
+    while (queue.isNotEmpty()) queue.removeFirst().apply {
+        if (c == endChar) return d
+        adjPoints(points, compare).filter { it.d == null }.forEach { it.d = d!! + 1; queue.addLast(it) }
     }
     return null
 }
